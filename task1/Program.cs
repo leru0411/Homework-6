701f784e39c4b8ac65bdc41dd808c8f5f57b8909
Valeriya <leru0411@mail.ru>
2022-07-14 20:21:30 +0300

    done homework number 6

---
+++ task1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+int[] GenerateArray(int length, string message)
+{
+    int[] userNumbers = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        userNumbers[i] = Prompt(message);
+    }
+    return userNumbers;
+}
+
+int CountNumbers(int[] array)
+{
+    int count = 0;
+    for (int i=0; i<array.Length; i++)
+    {
+        if (array[i]>0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int M = Prompt("Введите количество элементов: ");
+int[] myArray = GenerateArray(M, "Введите число: ");
+int counts = CountNumbers(myArray);
+System.Console.WriteLine($"Количество введнных элементов больше нуля равно {counts}");
---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//формула нахождения точки пересечения: x = (b2-b1)/k1-k2
+double Prompt(string message)
+{
+    System.Console.Write(message);
+    return double.Parse(Console.ReadLine());
+}
+double b1 = Prompt("Введите точку b1: ");
+double k1 = Prompt("Введите точку k1: ");
+double b2 = Prompt("Введите точку b2: ");
+double k2 = Prompt("Введите точку k1: ");
+
+System.Console.WriteLine($"Точка пересения двух прямых равна {(b2-b1)/(k1-k2)}");
